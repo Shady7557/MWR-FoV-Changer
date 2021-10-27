8e73997004be8ca4c213ed961e038e7a541d10d1
Shady <12400418+Shady7557@users.noreply.github.com>
2021-10-27 15:39:34 -0400

    do logging

---
+++ MWR FoV Changer/PathInfos.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class PathInfos
+{
+    public static readonly string AppData = Environment.GetEnvironmentVariable("appdata");
+    public static readonly string AppDataPath = AppData + @"\MWR FoV Changer";
+    public static readonly string LogsPath = AppDataPath + @"\Logs";
+}
+
--- MWR FoV Changer/Program.cs
+++ MWR FoV Changer/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CurtLog;
+using System;
+using System.Text;
 using System.Windows.Forms;
 
 namespace MWR_FoV_Changer
 {
     internal static class Program
     {
+        private static readonly StringBuilder _stringBuilder = new StringBuilder();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,6 +18,17 @@ namespace MWR_FoV_Changer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void InitLog()
+        {
+            Log.Settings.CustomLogHeader = _stringBuilder.Clear().Append("===========").Append(Application.ProductName).Append(" Log File===========").ToString();
+            Log.Settings.PerformanceOptions = Log.Performance.StandardPerformance;
+            Log.Settings.LogHeaderOptions = Log.LogHeader.CustomHeader;
+
+            var logLocation = _stringBuilder.Clear().Append(PathInfos.LogsPath).Append(@"\CFC_").Append(DateTime.Now.ToString("d")).Replace("/", "-").Append(".log").ToString();
+            Log.InitializeLogger(logLocation);
+
         }
     }
 }
