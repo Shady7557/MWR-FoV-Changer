ebd25061c0090c272eafdfdc2e25ac0664eeafd7
Shady <12400418+Shady7557@users.noreply.github.com>
2021-10-27 15:21:24 -0400

    Add project files.

---
+++ MWR FoV Changer/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace MWR_FoV_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Cur fov:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(197, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Cinematic Mode";
+            this.toolTip1.SetToolTip(this.checkBox1, "Attempts to not change the FoV when FoV is < 65");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "FoV:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(47, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            102,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            65,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            65,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(148, 121);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(130, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Viewmodel FoV = FoV";
+            this.toolTip1.SetToolTip(this.checkBox2, "Attempts to not change the FoV when FoV is < 65");
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Cur lod:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Viewmodel FoV:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(91, 36);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            102,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            65,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown3.TabIndex = 17;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            65,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 140);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ MWR FoV Changer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+using ReadWriteMemory;
+
+namespace MWR_FoV_Changer
+{
+    public partial class Form1 : Form
+    {
+        ProcessMemory mwr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoFoV();
+        }
+
+        ProcessMemory getProcessMemory(string processName)
+        {
+            if (string.IsNullOrEmpty(processName)) return null;
+            if (processName.Contains(".exe")) processName = processName.Replace(".exe", string.Empty);
+            var mem = new ProcessMemory(processName);
+            if (mem != null && mem.CheckProcess())
+            {
+                mem.StartProcess();
+                return mem;
+            }
+            return null;
+        }
+
+
+        int getIntPointerAddress(IntPtr baseAddress, int offset, ProcessMemory procMem)
+        {
+            if (procMem == null) return 0;
+            var pointer = 0;
+            var pointedTo = BitConverter.ToInt32(procMem.ReadMem(baseAddress.ToInt32(), 4), 0);
+            pointer = IntPtr.Add((IntPtr)Convert.ToInt32(pointedTo.ToString("X"), 16), offset).ToInt32();
+            return pointer;
+        }
+
+        void DoFoV()
+        {
+            try
+            {
+                if (mwr == null || !mwr.CheckProcess()) mwr = getProcessMemory("h1_sp64_ship");
+                if (mwr == null)
+                {
+                    label3.Text = "Status: failed to find process";
+                    return;
+                }
+
+                var fov = 65f;
+                var vmFoV = fov;
+                if (!float.TryParse(numericUpDown1.Value.ToString(), out fov)) return;
+                if (!float.TryParse(numericUpDown3.Value.ToString(), out vmFoV)) return;
+
+                var FoVAddr = 0x14C234A70;
+                var vmFoVAddr = 0x14C234D70;
+
+                var currentFoV = BitConverter.ToSingle(mwr.ReadMem(FoVAddr, 4), 0);
+                var currentVMFoV = BitConverter.ToSingle(mwr.ReadMem(vmFoVAddr, 4), 0);
+                if (checkBox1.Checked && currentFoV <= 64)
+                {
+                    label3.Text = "Status: not changing fov because cinematic mode";
+                    return;
+                }
+                mwr.WriteFloat(FoVAddr, fov);
+                mwr.WriteFloat(vmFoVAddr, vmFoV);
+                label3.Text = "Status: Found process and wrote to memory";
+                label2.Text = "Cur fov: " + BitConverter.ToSingle(mwr.ReadMem(FoVAddr, 4), 0) + " VM: " + BitConverter.ToSingle(mwr.ReadMem(vmFoVAddr, 4), 0);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Failed to complete DoFoV(): " + Environment.NewLine + ex.ToString());
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked) numericUpDown3.Value = numericUpDown1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            DoFoV();
+            if (checkBox2.Checked) numericUpDown3.Value = numericUpDown1.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DoFoV();
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            DoFoV();
+        }
+    }
+}
